4a554c7c36758cc990525a6c0fdaedf01a3271a3
Rop Gonggrijp <rop@gonggri.jp>
2020-09-21 14:00:49 +0200

    Updated the example sketch

--- examples/Basics/touch/touch.ino
+++ examples/Basics/touch/touch.ino
@@ -1,24 +1,43 @@
 #include <M5Core2.h>
 
+TouchZone topHalf(0,0,320,120);
+TouchZone bottomHalf(0,120,320,160);
+Gesture swipeDown(topHalf, bottomHalf, "Swipe Down");
+
+TouchButton lt = TouchButton(0, 0, 160, 120, "left-top");
+TouchButton lb = TouchButton(0, 120, 160, 120, "left-bottom");
+TouchButton rt = TouchButton(160, 0, 160, 120, "right-top");
+TouchButton rb = TouchButton(160, 120, 160, 120, "right-bottom");
+
 void setup() {
   M5.begin();
+  M5.Touch.addHandler(eventDisplay);
+  M5.Touch.addHandler(colorButtons, TE_BTNONLY + TE_TOUCH + TE_RELEASE);
+  swipeDown.addHandler(yayWeSwiped);
+  rt.addHandler(dblTapped, TE_DBLTAP);
 }
 
 void loop() {
   M5.update();
-  if (M5.Touch.hasChanged()) {
-    if (!M5.Touch.points) Serial.print("--");
-    if (M5.Touch.points) Serial.printf("x: %d, y: %d        ",  M5.Touch.point[0].x, M5.Touch.point[0].y);
-    if (M5.Touch.points == 2) Serial.printf("x: %d, y: %d",  M5.Touch.point[1].x, M5.Touch.point[1].y);
-    Serial.println();
+}
+
+void eventDisplay(TouchEvent& e) {
+  Serial.printf("%-12s finger%d  %-18s (%3d, %3d)", M5.Touch.eventTypeName(e), e.finger, M5.Touch.eventObjName(e),  e.from.x, e.from.y);
+  if (e.type != TE_TOUCH && e.type != TE_TAP && e.type != TE_DBLTAP) {
+    Serial.printf("--> (%3d, %3d)  %5d ms", e.to.x, e.to.y, e.duration);
   }
+  Serial.println();
 }
 
-void btnHandler(TouchButton &btn) {
-  M5.Lcd.fillRect(btn.x0, btn.y0, btn.x1 - btn.x0, btn.y1 - btn.y0, btn.isPressed() ? WHITE : BLACK);
+void colorButtons(TouchEvent& e) {
+  TouchButton& b = *e.button;
+  M5.Lcd.fillRect(b.x, b.y, b.w, b.h, b.isPressed() ? WHITE : BLACK);
 }
 
-TouchButton lt = TouchButton(0, 0, 159, 119, btnHandler, btnHandler);
-TouchButton lb = TouchButton(0, 120, 159, 240, btnHandler, btnHandler);
-TouchButton rt = TouchButton(160, 0, 320, 119, btnHandler, btnHandler);
-TouchButton rb = TouchButton(160, 120, 320, 240, btnHandler, btnHandler);
+void yayWeSwiped(TouchEvent& e) {
+  Serial.println("--- SWIPE DOWN DETECTED ---");
+}
+
+void dblTapped(TouchEvent& e) {
+  Serial.println("--- TOP RIGHT BUTTON WAS DOUBLETAPPED ---");
+}
