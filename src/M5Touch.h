6e467671ed3985f9bbe4eb7cac67946f8b26d21a
Rop Gonggrijp <rop@gonggri.jp>
2020-10-05 00:13:45 +0200

    Touch library now fully documented in the header file

--- src/M5Touch.h
+++ src/M5Touch.h
@@ -1,3 +1,230 @@
+/*
+
+M5Touch - The M5Stack Core2 Touch Library
+
+    This is the library behind the M5.Touch object that you can use to read
+    from the touch sensor on the M5Stack Core2. It was made to be an input
+    source for the M5Button library that provides higher level buttons,
+    gestures and events, but both libraries can be also be used alone.
+
+
+
+About the Touch Sensor in the M5Stack Core2
+
+    Touch is provided by a FocalTech FT6336 chip, which supports two
+    simultaneous touches. However, the M5Stack Core2 touch display is only
+    multi-touch in one dimension. What that means is that it can detect two
+    separate touches only if they occur on different vertical positions.
+    This has to do with the way the touch screen is wired, it's not
+    something that can be changed in software. So you will only ever see
+    two points if they do not occur side-by-side. Touches that do happen
+    side-by-side blend into one touch that is detected somewhere between
+    the actual touches.
+
+    While this limits multi-touch somewhat, you can still create multiple
+    buttons and see two that are not on the same row simultaneously. You
+    could also use one of the buttons below the screen as a modifier for
+    something touched on the screen.
+
+    The touch sensor extends to below the screen of the Core2: the sensor
+    is 320x280 pixels, the screen is 320x240. The missing 40 pixels are
+    placed below the screen, where the printed circles are. This is meant
+    to simulate the three hardware buttons on the original M5Stack units.
+    Note that on at least some units the touch sensor in this area only
+    operates properly if the USB cable is plugged in or if the unit is
+    placed on a metal surface.
+
+
+
+Point and Zone: Describing Points and Areas on the Screen
+
+    The Point and Zone classes allow you to create variables that hold a
+    point or an area on the screen. You can
+
+    Point
+        Holds a point on the screen. Has members x and y that hold the
+        coordinates of a touch. Values -1 for x and y indicate an invalid
+        touch value.
+
+    Zone
+        Holds a rectangular area on the screen. Members x, y, w and h are
+        for the x and y coordinate of the top-left corner and the width and
+        height of the rectangle.
+
+    The 'valid' method tests if a point is valid. Using the 'in' or
+    'contains' method you can test if a point is in a zone. There is also
+    a 'distanceTo' method to see how far two points are apart. Here are some
+    examples to make things clear:
+
+        Point a;
+        Point b(50, 120);
+        Serial.println(a.valid());          // 0
+        Serial.println(a);                  // (-1, -1)
+        a.set(10, 30);
+        Serial.println(a.valid());          // 1
+        Serial.println(b.y);                // 120
+        Zone z(0,0,100, 100);
+        Serial.println(z.w);                // 100
+        Serial.println(z.contains(a));      // 1
+        Serial.println(b.in(z));            // 0
+        Serial.println(a.distanceTo(b));    // 98
+
+
+
+Basic Touch API
+
+    The basic API provides a way to read the data from the touch sensor.
+    While you may want to use this directly. But you may want to skip using
+    this API as even for simple applications the more advanced ways of
+    using the touch sensor are easier.
+
+    M5.update()
+        In the loop() part of your sketch, call "M5.update()". This will in
+        turn call M5.Touch.update(), which is the only part that talks to
+        the touch interface. It updates the data used by the rest of the
+        API.
+
+    M5.Touch.changed
+        Is true if anything changed on the touchpad since the last time
+        M5.update() was called.
+
+    M5.Touch.points
+        Contains the number of touches detected: 0, 1 or 2.
+
+    M5.Touch.point[0], M5.Touch.point[1]
+        M5.Touch.point[0] and M5.Touch.point[1] are Points that hold the
+        detected touches.
+
+
+    A very simple sketch to print the location where the screen is touched:
+
+        #include <M5Core2.h>
+
+        void setup() {
+          M5.begin();
+        }
+
+        void loop() {
+          M5.update();
+          if ( M5.Touch.changed ) Serial.println( M5.Touch.point[0] );
+        }
+
+
+
+Buttons, Gestures, Events
+
+    Note tha you may not want to use any of the above directly. The
+    M5Buttons library provides Button, Gesture and Events objects that
+    allow you to quickly create reactive visual buttons on the screen and
+    react differently based on whether a button was clicked, tapped, or
+    double-tapped. Have a look at the documentation for that, which is in
+    the M5Button.h file in the src/utility directory of this repository.
+    The example under File / Examples / M5Core2 / Basics / touch in your
+    Arduino environment should give you an idea of what's possible.
+
+
+
+Screen Rotation
+
+    If you rotate the screen with M5.Lcd.setRotation, the touch coordinates
+    will rotate along with it.
+
+    * What that means is that either x or y for the area below the screen
+      may go negative. Say you use the screen upside-down with
+      M5.Lcd.setRotation(3). In that case the off-screen touch area (Y
+      coordinates 240 through 279) that was below the screen now becomes
+      above the screen and has Y coordinates -40 through -1.
+
+    * See the button library for a feature that allows you to keep some
+      Zone and Button objects in the same place on the physical screen,
+      regardless of rotation.
+
+    M5.Touch.rotation
+        Holds the current rotation, in the same format as the rotation for
+        the display. Is normally set by the M5Display when it receives
+        M5.Lcd.setRotation, but can be changed manually also.
+
+
+
+TFT_eSPI Resistive Touch API emulation
+
+    While technically not part of this library itself, we added an
+    emulation for the TFT_eSPI touch API to the M5Display object that
+    merely passes informaton on to the M5.Touch object. So M5.Lcd can be
+    addressed as if it's a touch screen using that older resistive touch
+    interface. Do note that this interface is not nearly as powerful as
+    M5.Touch's native API. But together with M5Button's TFT_eSPI_Button
+    emulation, this should allow you to compile lots of ESP32 software
+    written for touch screens.
+
+
+Advanced Uses of the Touch Library
+
+    You should never need any of the below features in everyday use. But
+    they're there just in case...
+
+    M5.Touch.wasRead
+        True if the sensor was actually read. The sensor can only provide
+        updates every 13 milliseconds or so. M5.update() can loop as quick
+        as once every 20 MICROseconds, meaning it would continually read
+        the sensor when there was nothing to read. So M5.Touch.read() only
+        really reads when it's time to do so, and returns with
+        M5.Touch.wasRead false otherwise.
+
+    M5.point0finger
+        The FT6336 chip keeps track of fingers, each touch has a finger
+        ID of 0 or 1. So when there are two touches in point[0] and
+        point[1] and then one goes away, point0finger allows you to see
+        which touch is left in point[0].
+
+    M5.Touch.interval()
+        Without arguments returns the current interval between sensor
+        updates in milliseconds. If you supply a number that's the new
+        interval. The default of 13 seems to give the most updates per
+        second.
+
+    M5.Touch.ft6336(reg)
+    M5.Touch.ft6336(reg, value)
+    M5.Touch.ft6336(reg, size, *data)
+        Allows you to read and write registers on the ft6336 touch
+        interface chip directly. The first form reads one byte, the second
+        form writes one and the third form reads a block of 'size' bytes
+        starting at 'reg' into a buffer at '*data'.
+
+    M5.Touch.dump()
+        M5.Touch.dump() dumps the entire register space on the FT6336 chip
+        as a formatted hexdump to the serial port.
+
+
+
+
+Legacy API
+
+    There was a previous version of this library, and it provided a number
+    of functions that were single touch only. The older version did not
+    have M5.update(). Instead it used ispressed() and getPressedPoint()
+    functions as well as HotZones, which provided something that worked a
+    little bit like Buttons. This older API is still supported (the
+    M5Core2 Factory Test sketch still works), but you should not use it
+    for new programs. The ispressed() function specifically does not mix
+    well with code that uses M5.update().
+
+
+
+Example
+
+    It may sound complicated when you read it all in this document, but
+    it's all made to be easy to use.
+
+    Under File / Examples / M5Core2 / Basics / touch in your Arduino
+    environment is an example sketch called "touch" that shows both this
+    library and M5Button in action. Please have a look at it to understand
+    how this all works and run the sketch to see all the events printed to
+    the serial port. It shows buttons, gestures and events and should be
+    pretty self-explanatory.
+
+*/
+
 #ifndef _M5TOUCH_H_
 #define _M5TOUCH_H_
 
