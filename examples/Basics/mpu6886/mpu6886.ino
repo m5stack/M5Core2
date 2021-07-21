95f304baad4047ce76a96ba46ee630053abda007
Tinyu <Tinyu.Zhao@gmail.com>
2021-07-21 18:17:28 +0800

    update some example

--- examples/Basics/mpu6886/mpu6886.ino
+++ examples/Basics/mpu6886/mpu6886.ino
@@ -1,7 +1,19 @@
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core2 sample source code
+*                          配套  M5Core2 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/core2
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/core2
+*
+* describe：MPU6886 example.  惯性传感器
+* date：2021/7/21
+*******************************************************************************
+*/
 #include <M5Core2.h>
 
-float accX = 0.0F;
-float accY = 0.0F;
+float accX = 0.0F;  // Define variables for storing inertial sensor data
+float accY = 0.0F;  //定义存储惯性传感器相关数据的相关变量
 float accZ = 0.0F;
 
 float gyroX = 0.0F;
@@ -14,41 +26,44 @@ float yaw   = 0.0F;
 
 float temp = 0.0F;
 
-// the setup routine runs once when M5Stack starts up
+/* After M5Core2 is started or reset
+the program in the setUp () function will be run, and this part will only be run once.
+在 M5Core2 启动或者复位后，即会开始执行setup()函数中的程序，该部分只会执行一次。 */
 void setup(){
-
-  // Initialize the M5Stack object
-  M5.begin();
-  
-  M5.IMU.Init();
-
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextColor(GREEN , BLACK);
-  M5.Lcd.setTextSize(2);
+  M5.begin(); //Init M5Core.  初始化 M5Core
+  M5.IMU.Init();  //Init IMU sensor.  初始化惯性传感器
+  M5.Lcd.fillScreen(BLACK); //Set the screen background color to black. 设置屏幕背景色为黑色
+  M5.Lcd.setTextColor(GREEN , BLACK); //Sets the foreground color and background color of the displayed text.  设置显示文本的前景颜色和背景颜色
+  M5.Lcd.setTextSize(2);  //Set the font size.  设置字体大小
 }
 
-// the loop routine runs over and over again forever
-void loop() {
-    // put your main code here, to run repeatedly:
-  M5.IMU.getGyroData(&gyroX,&gyroY,&gyroZ);
-  M5.IMU.getAccelData(&accX,&accY,&accZ);
-  M5.IMU.getAhrsData(&pitch,&roll,&yaw);
-  M5.IMU.getTempData(&temp);
-  
-  M5.Lcd.setCursor(0, 20);
-  M5.Lcd.printf("%6.2f  %6.2f  %6.2f      ", gyroX, gyroY, gyroZ);
-  M5.Lcd.setCursor(220, 42);
-  M5.Lcd.print(" o/s");
-  M5.Lcd.setCursor(0, 65);
-  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", accX, accY, accZ);
-  M5.Lcd.setCursor(220, 87);
-  M5.Lcd.print(" G");
-  M5.Lcd.setCursor(0, 110);
-  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", pitch, roll, yaw);
-  M5.Lcd.setCursor(220, 132);
-  M5.Lcd.print(" degree");
-  M5.Lcd.setCursor(0, 155);
+/* The M5Core screen is 320x240 pixels, starting at the top left corner of the screen (0,0).
+gyroscope output related
+M5Stack屏幕像素为 320x240,以屏幕左上角为原点 (0,0)*/
+//gyroscope output related.  陀螺仪输出相关
+  M5.Lcd.setCursor(0, 20);  //Move the cursor position to (x,y).  移动光标位置到(x,y)处
+  M5.Lcd.printf("gyroX,  gyroY, gyroZ"); //Screen printingformatted string.  输出格式化字符串
+  M5.Lcd.setCursor(0, 42);
+  M5.Lcd.printf("%6.2f %6.2f%6.2f o/s", gyroX, gyroY, gyroZ);
+
+// Accelerometer output is related
+//加速度计输出相关
+  M5.Lcd.setCursor(0, 70);
+  M5.Lcd.printf("accX,   accY,  accZ");
+  M5.Lcd.setCursor(0, 92);
+  M5.Lcd.printf("%5.2f  %5.2f  %5.2f G", accX, accY, accZ);
+
+// Pose output is related
+//姿态输出相关
+  M5.Lcd.setCursor(0, 120);
+  M5.Lcd.printf("pitch,  roll,  yaw");
+  M5.Lcd.setCursor(0, 142);
+  M5.Lcd.printf("%5.2f  %5.2f  %5.2f deg", pitch, roll, yaw);
+
+// Inertial sensor temperature output related
+//惯性传感器温度输出相关
+  M5.Lcd.setCursor(0, 175);
   M5.Lcd.printf("Temperature : %.2f C", temp);
 
-  delay(1);
+  delay(1000);  // Delay 1000ms (1 sec)  //延迟1000ms（1秒）
 }
--- examples/Basics/sleep/sleep.ino
+++ examples/Basics/sleep/sleep.ino
@@ -1,19 +1,33 @@
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core2 sample source code
+*                          配套  M5Core2 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/core2
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/core2
+*
+* describe：Sleep--Axp电源休眠
+* date：2021/7/21
+*******************************************************************************
+*/
 #include <M5Core2.h>
 
-void setup()
-{
-  M5.begin();
+/* After M5Core2 is started or reset
+the program in the setUp () function will be run, and this part will only be run once.
+在 M5Core2 启动或者复位后，即会开始执行setup()函数中的程序，该部分只会执行一次。 */
+void setup(){
+  M5.begin(); //Init M5Core2.  初始化 M5Core2
 
-  M5.Lcd.setTextFont(2);
+  M5.Lcd.setTextFont(2);  //Set font size to 2.  设置字体大小为2
 
-  Serial.println("Light / Deep Sleep Test.");
-  M5.Lcd.println("Light / Deep Sleep Test.");
+  Serial.println("Light / Deep Sleep Test."); //The serial port prints the formatted string with a newline.  串口输出格式化字符串并换行
+  M5.Lcd.println("Light / Deep Sleep Test."); //The screen prints the formatted string and wraps it.  屏幕打印格式化字符串并换行
 
   Serial.println("Going to light sleep for 5 seconds.");
   M5.Lcd.println("Going to light sleep for 5 seconds.");
-  delay(2500);
+  delay(2500);  //delay 2500ms.  延迟2500ms
 
-  M5.Axp.LightSleep(SLEEP_SEC(5));
+  M5.Axp.LightSleep(SLEEP_SEC(5));  //Wake up after 5 seconds of light sleep, the CPU will reboot and the program will start from the beginning.  轻度睡眠10秒后重新启动，程序从下一行继续执行
 
   Serial.println("Wakeup from light sleep.");
   M5.Lcd.println("Wakeup from light sleep.");
@@ -23,8 +37,7 @@ void setup()
   M5.Lcd.println("Going to deep sleep for 5 seconds.");
   delay(2500);
 
-  M5.Axp.DeepSleep(SLEEP_SEC(5));
-  // Never reached
+  M5.Axp.DeepSleep(SLEEP_SEC(5)); //Wake up after 5 seconds of light sleep, the CPU will reboot and the program will start from the beginning.  轻度睡眠5秒后唤醒,CPU将重新启动，程序将从头开始执行
 }
 
 void loop()
