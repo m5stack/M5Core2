077a43e6d67b5a8720c46c32938e950dd08171c4
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-10 09:21:31 +0800

    button pin

--- examples/Unit/BUTTON/BUTTON.ino
+++ examples/Unit/BUTTON/BUTTON.ino
@@ -9,9 +9,10 @@
 * describe：Button.  按键
 * date：2021/7/26
 *******************************************************************************
-  Please connect to Port A,Read the button status of BUTTON Unit and display it on the screen
-
-  请连接端口A,读取按键的状态并在显示屏上显示
+  Please connect to Port A(36),Read the button status of BUTTON Unit and display it on the screen
+  请连接端口A(36),读取按键的状态并在显示屏上显示
+  if you don't have M5GO BOTTOM, you need change the pinMode and the digitalRead to 33, But you will not be able to use any I2C operations.
+  如果你没有M5GO BOTTOM，你需要改变pinMode和digitalRead到33,但是你将不能使用任何I2C操作.
 */
 #include <M5Core2.h>
 
