4132fa0af4bc260748addb149c83e86d98f4c285
Tinyu <Tinyu.Zhao@gmail.com>
2021-08-10 15:56:50 +0800

    relay

--- examples/Unit/4-RELAY/4-RELAY/4-RELAY.ino
+++ examples/Unit/4-RELAY/4-RELAY/4-RELAY.ino
@@ -1,121 +1,88 @@
 /*
-    Description: Control 4 relays and demonstrate the asynchronous control relay LED
-*/
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5Core2 sample source code
+*                          配套  M5Core2 示例源代码
+* Visit the website for more information：https://docs.m5stack.com/en/core/core2
+* 获取更多资料请访问：https://docs.m5stack.com/zh_CN/core/core2
+*
+* describe：4-Relay.  4个继电器
+* date：2021/8/9
+*******************************************************************************
+  Please connect to port A,Control 4 relays and demonstrate the asynchronous control relay LED
+  请连接A端口，控制4继电器，并演示异步控制继电器LED
+-------------------------------------------------------------------------------
+  RELAY control reg           | 0x10
+  -----------------------------------------------------------------------------
+  Relay_ctrl_mode_reg[0]      | R/W | System control
+                              | 7 | 6 | 5 | 4 | 3 | 2 | 1 |     0     |
+                              | R | R | R | R | R | R | R | Sync Mode |
+                              | -Sync Mode:0 LED&Relay Async
+                              | -Sync Mode:1 LED&Relay Sync
+  ---------------------------------------------------------------------------------
+  Relay_ctrl_mode_reg[1]      | R/W | Relay & LED control
+                              |  7  |  6  |  5  |  4  |  3  |  2  |  1  |  0  |
+                              | LED1| LED2| LED3| LED4| RLY1| RLY2| RLY3| RLY4|
+-------------------------------------------------------------------------------*/
 
 #include <M5Core2.h>
-
-/*-----------------------------------------------------------------------------*/
-// |RELAY control reg          | 0x10
-// |-----------------------------------------------------------------------------
-// |Relay_ctrl_mode_reg[0]     | R/W | System control
-//                                   | 7 | 6 | 5 | 4 | 3 | 2 | 1 |     0     |
-//                                   | R | R | R | R | R | R | R | Sync Mode |
-//                                   | -Sync Mode:0 LED&Relay Async
-//                                   | -Sync Mode:1 LED&Relay Sync
-//---------------------------------------------------------------------------------
-// |Relay_ctrl_mode_reg[1]     | R/W | Relay & LED control
-//                             |  7  |  6  |  5  |  4  |  3  |  2  |  1  |  0  |
-//                             | LED1| LED2| LED3| LED4| RLY1| RLY2| RLY3| RLY4|
-//
-/*-------------------------------------------------------------------------------*/
-
-void WriteRelayReg( int regAddr, int data )
-{
-    Wire.beginTransmission(0x26);
-    Wire.write(regAddr);
-    Wire.write(data);
-    Wire.endTransmission();
-    Serial.printf("[ W ] %02X : %02X. \r\n", regAddr, data);
-}
-
-int readRelayReg(int regAddr)
-{
-    Wire.beginTransmission(0x26);
-    Wire.write(regAddr);
-    Wire.endTransmission();
-    Wire.requestFrom(0x26, 1);
-    int data = Wire.read() & 0x00ff;
-    Serial.printf("[ R ] %02X : %02X. \r\n", regAddr, data);
-    return data;
-}
-
-void WriteRelayNumber( int number, int state )
-{
-    int StateFromDevice = readRelayReg(0x11);
-    if( state == 0 )
-    {
-        StateFromDevice &= ~( 0x01 << number );
-    }
-    else
-    {
-        StateFromDevice |= ( 0x01 << number );
-    }
-    WriteRelayReg(0x11,StateFromDevice);
-}
+#include "UNIT_4RELAY.h"
+UNIT_4RELAY unit_4relay;
 
 void setup() {
-  // put your setup code here, to run once:
-  M5.begin(true, true, true, true);
-  M5.Lcd.setCursor(90, 0, 4);
-  M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
-  M5.Lcd.print("4-RELAY UNIT");
+  M5.begin(true,false,true,true); //Init M5Core And the I2C port(21,22).  初始化 M5Core 和I2C(21,22)端口
+  M5.Lcd.setCursor(80, 0, 4); //Set the cursor position to (80,0).  将光标位置设置为(80,0)
+  M5.Lcd.print("4-RELAY UNIT\n\n");
+  M5.Lcd.print("   Relay State: \n   Sync Mode: ");
   M5.Lcd.setCursor(0, 220, 2);
-  M5.Lcd.print("Independent Switch");
-  M5.Lcd.setCursor(130, 220, 2);
-  M5.Lcd.print("LED Sync/Async");
-  M5.Lcd.setCursor(250, 220, 2);
-  M5.Lcd.print("ALL Relay");
-  M5.Lcd.setCursor(20, 50, 4);
-  M5.Lcd.print("Relay State: ");
-  M5.Lcd.setCursor(20, 80, 4);
-  M5.Lcd.print("Sync Mode: ");
-  readRelayReg(0x10);
-  readRelayReg(0x11);
-  WriteRelayReg(0x10,1);
-  WriteRelayReg(0x11,0);
-  //WriteRelayNumber(0,0);
+  M5.Lcd.print("Independent switch  Sync/Async    ALL relay");
+  unit_4relay.Init(0);  //Set the lamp and relay to asynchronous mode(Async = 0,Sync = 1).  将灯和继电器设为非同步模式
 }
 
-int count_i = 0;
-bool flag_led, flag_relay = false;
-
+char count_i = 0;
+bool sync_flag = 0, all_flag= 0;
 
 void loop() {
-    if(M5.BtnA.wasPressed()){
-      M5.Lcd.fillRect(160, 50, 100, 20, TFT_BLACK);
-      M5.Lcd.setCursor(160, 50, 4);
-      M5.Lcd.printf("%d ON", count_i);
-      WriteRelayReg(0x11,(0x01 << count_i));
-      count_i++;    
-      if( count_i >= 4 ) count_i = 0; 
+  M5.update();  //Check button down state.  检测按键按下状态
+  if(M5.BtnA.wasPressed()){ //If button A is pressed.  如果按键A按下
+    M5.Lcd.fillRect(160, 50, 100, 20, BLACK);
+    M5.Lcd.setCursor(160, 50, 4);
+    if(count_i<4){
+      M5.Lcd.printf("%d ON", count_i+1);
+      if(sync_flag){
+        unit_4relay.relayWrite(count_i,1);  //Open the relay at Count_i.  打开count_i处的继电器
+      }else unit_4relay.LEDWrite(count_i,1);  //Turn on count_I to get led lights.  打开count_i出得led灯
+    }else{
+      M5.Lcd.printf("%d OFF", (count_i-3));
+      if(sync_flag){
+        unit_4relay.relayWrite((count_i-4),0);  //Close the relay at Count_i.  关闭count_i处的继电器
+      }else unit_4relay.LEDWrite((count_i-4),0);  //Turn off the COUNt_I leds.  关闭count_i出得led灯
     }
-    if(M5.BtnB.wasPressed()){
-      M5.Lcd.fillRect(160, 80, 100, 20, TFT_BLACK);
-      if(!flag_led){
-        M5.Lcd.setCursor(160, 80, 4);
-        M5.Lcd.print("Async");
-        WriteRelayReg(0x10, 0);
-      }else {
-        M5.Lcd.setCursor(160, 80, 4);
-        M5.Lcd.print("Sync");
-        WriteRelayReg(0x10, 1);
-      }
-      flag_led = !flag_led;     
+    count_i++;
+    if(count_i >= 8) count_i = 0;
+  }else if(M5.BtnB.wasPressed()){
+    sync_flag = !sync_flag;
+    unit_4relay.switchMode(sync_flag);
+    M5.Lcd.fillRect(160, 80, 100, 20, BLACK);
+    M5.Lcd.setCursor(160, 80, 4);
+    if(!sync_flag){
+      M5.Lcd.print("Async");
+    }else {
+      M5.Lcd.print("Sync");
     }
-    if(M5.BtnC.wasPressed()){
-        M5.Lcd.fillRect(160, 50, 100, 20, TFT_BLACK);
-        for(int i=0; i<4; i++){
-          if(!flag_relay) {
-            M5.Lcd.setCursor(160, 50, 4);
-            M5.Lcd.print("ON");
-            WriteRelayNumber(i, 1);    
-          }else {
-            M5.Lcd.setCursor(160, 50, 4);
-            M5.Lcd.print("OFF");
-            WriteRelayNumber(i, 0); 
-          }
-        }
-        flag_relay = !flag_relay;
+  }
+  if(M5.BtnC.wasPressed()){
+    all_flag = !all_flag;
+    M5.Lcd.fillRect(160, 50, 100, 20, BLACK);
+    M5.Lcd.setCursor(160, 50, 4);
+    if(all_flag){
+      M5.Lcd.printf("ALL.ON ");
+      if(sync_flag) unit_4relay.relayALL(1);  //Open all the relays.  打开所有的继电器
+      else unit_4relay.LED_ALL(1);  //Turn on all the lights.  打开所有的灯
+    }else{
+      M5.Lcd.printf("ALL.OFF");
+      if(sync_flag)unit_4relay.relayALL(0); //Close all relays.  关闭所有的继电器
+      else unit_4relay.LED_ALL(0);  //Turn off all the lights.  关闭所有的灯
     }
-    M5.update();
+  }
 }
