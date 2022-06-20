dbc63c76311e0f6a62a3aef19014a314afe37749
Forairaaaaa <applesyqd@outlook.com>
2022-06-20 12:23:48 +0800

    Update LIMIT.ino

--- examples/Unit/LIMIT/LIMIT.ino
+++ examples/Unit/LIMIT/LIMIT.ino
@@ -3,8 +3,8 @@
 * Copyright (c) 2022 by M5Stack
 *                  Equipped with M5Core2 sample source code
 *                          配套  M5Core2 示例源代码
-* Visit the website for more information:
-* 获取更多资料请访问:
+* Visit the website for more information: https://docs.m5stack.com/zh_CN/products
+* 获取更多资料请访问: https://docs.m5stack.com/zh_CN/products
 *
 * describe: Limit.
 * date: 2022/6/1
@@ -13,7 +13,7 @@
 
 #include <M5Core2.h>
 
-#define KEY_PIN 33  // Define Limit Pin.  定义Limit连接引脚
+#define KEY_PIN 36  // Define Limit Pin.  定义Limit连接引脚
 
 void setup() {
     M5.begin();  // Init M5Stack  初始化M5Stack
